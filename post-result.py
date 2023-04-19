db6d92055355f6f1000d6baf24a0c08b8a9099f6
Andy McKay <andy@clearwind.ca>
2023-04-19 15:21:21 -0700

    Update post-result.py

--- post-result.py
+++ post-result.py
@@ -1,8 +1,12 @@
 import requests
 import os
+import base64
+import json
 
 token = os.getenv("SERVICE_CATALOG_TOKEN")
 payload = os.getenv("PAYLOAD")
+payload = base64.decode('utf-8')
+payload = json.load(payload)
 url = payload["server"]["url"] + payload["server"]["endpoint"]
 data = {"result": "pass"}
 headers = {"Authorization": f"Token {token}"}
